4699ffe165a55c621abeacf9e64981248fa62b42
Kaushik Mukherjee <k.b.mukherjee@accenture.com>
2017-04-07 07:12:31 -0500

    .

--- src/main/java/example/nosql/UserServlet.java
+++ src/main/java/example/nosql/UserServlet.java
@@ -1,5 +1,28 @@
 package example.nosql;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.String;
+import java.lang.StringBuilder;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.cloudant.client.api.Database;
+import com.cloudant.client.api.model.Document;
+import com.cloudant.client.api.model.Params;
+import com.cloudant.client.org.lightcouch.Attachment;
+import com.google.gson.JsonObject;
+
 @WebServlet("/User")
 public class UserServlet extends HttpServlet {
 
